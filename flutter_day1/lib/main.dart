1c66addb8398c3d37121bcc0d2e8faf575cd0950
kurokyfan <kuro2007cumt@gamil.com>
2019-03-18 17:43:39 +0800

    update

--- flutter_day1/lib/main.dart
+++ flutter_day1/lib/main.dart
@@ -71,35 +71,11 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return getCard();
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -108,4 +84,78 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  /// 返回一个居中带图标和文本的item
+  getBottomItem(IconData icon, String, text) {
+    // 充满 row 横向的布局
+    return Expanded(
+      flex: 1,
+      // 居中显示
+      child: Center(
+        // 横向布局
+        child: Row(
+          // 主轴居中，即是横向居中
+          mainAxisAlignment: MainAxisAlignment.center,
+          // 大小按照最大充满
+          mainAxisSize: MainAxisSize.max,
+          // 竖向也居中
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              icon,
+              size: 16.0,
+              color: Colors.grey,
+            ),
+            // 间隔
+            Padding(padding: EdgeInsets.only(left: 5.0)),
+            Text(
+              text,
+              // 设置字体颜色样式
+              style: TextStyle(color: Colors.grey, fontSize: 14.0),
+              // 超过的省略为...显示
+              overflow: TextOverflow.ellipsis,
+              // 最长一行
+              maxLines: 1,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  getCard() {
+    return Container(
+      // 卡片包装
+      child: Card(
+        // 增加点击效果
+        child: FlatButton(
+          onPressed: () { print('click'); },
+          child: Padding(
+            padding: EdgeInsets.only(left: 0.0, top: 10.0, right: 10.0, bottom: 10.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  child: Text('一点描述', style: TextStyle(color: Colors.grey, fontSize: 14.0), maxLines: 3, overflow: TextOverflow.ellipsis),
+                  margin: EdgeInsets.only(top: 6.0, bottom: 2.0),
+                  alignment: Alignment.topLeft,
+                ),
+                Padding(padding: EdgeInsets.all(10.0)),
+                Row(
+                  // 三个平均分配的横向图标文字
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    getBottomItem(Icons.star, String, "1000"),
+                    getBottomItem(Icons.link, String, "1000"),
+                    getBottomItem(Icons.subject, String, "1000")
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
 }
